15fa44458c62a5145ff87f1c39924a6623039405
hotuananh0609 <111572722+hotuananh0609@users.noreply.github.com>
2022-09-07 20:36:29 +0700

    Tang Qnhi nguuu

---
+++ Tuananhnee/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Quỳnh Nhi Tồyy!',
+    introDesc: `Bật hoạt động lên giùm cái!!!`,
+    btnIntro: 'hihi',
+    title: 'QNhi hư quá',
+    desc: 'Thích QNhi rồi làm sao đâyy? ',
+    btnYes: 'Thích lại <33',
+    btnNo: 'Không thích ',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu Hồ Tuấn Anh <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Giờ chơi game nè!',
+    btnAccept: 'Dạ <3',
+    messLink: 'https://www.facebook.com/hotuananh04/'
+}
